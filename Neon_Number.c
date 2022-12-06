3d4dc2e4c7d9b0c27f91019503c469685131923d
DivyaTejaP <119919278+DivyaTejaP@users.noreply.github.com>
2022-12-06 11:57:43 +0530

    #include<stdio.h> int main() {     int n,f;     scanf("%d",&n);     if(n==9 || n==1)     {         printf("Neon Number");     }     else     {         printf("Not Neon Number");     } }

---
+++ Neon_Number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,f;
+    scanf("%d",&n);
+    if(n==9 || n==1)
+    {
+        printf("Neon Number");
+    }
+    else
+    {
+        printf("Not Neon Number");
+    }
+}
