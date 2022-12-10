f25dd9241de9ce41905a35ddd763707d7759b48c
DivyaTejaP <119919278+DivyaTejaP@users.noreply.github.com>
2022-12-10 19:45:18 +0530

    #include<stdio.h> int main() {     int a,i,j,b,d;     scanf("%d",&a);     for(j=1;j<=a;j++)     {         for(i=a;i>=1;i--)         {             printf("%d ",i);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int a,i,j;
+    int a,i,j,b,d;
     scanf("%d",&a);
-    for(i=1;i<=a;i++)
+    for(j=1;j<=a;j++)
     {
-        for(j=1;j<=i;j++)
+        for(i=a;i>=1;i--)
         {
-            printf("%d",j);
+            printf("%d ",i);
         }
         printf("
 ");
