0fc7aa9c4bcd452ce20da6a3c91ccc8e2ac9f7c0
DivyaTejaP <119919278+DivyaTejaP@users.noreply.github.com>
2022-12-12 10:40:29 +0530

    #include<stdio.h> int main() {     int a,i,b,s=0,max=0;     scanf("%d",&a);     for(i=a;i>0;i=i/10)     {         b=i%10;         if(b>max)         {             max=b;         }     }     printf("%d",max); }

---
+++ Largest_Digit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,b,s=0,max=0;
+    scanf("%d",&a);
+    for(i=a;i>0;i=i/10)
+    {
+        b=i%10;
+        if(b>max)
+        {
+            max=b;
+        }
+    }
+    printf("%d",max);
+}
