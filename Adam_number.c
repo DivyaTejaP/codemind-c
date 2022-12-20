65b9d79747673f5c190cc58c6b25048bec5c815e
DivyaTejaP <119919278+DivyaTejaP@users.noreply.github.com>
2022-12-20 10:33:56 +0530

    #include<stdio.h> int main() {     int a,b,c,i,s=0,d,j,x,y=0;     scanf("%d",&a);     b=a*a;     for(i=a;i>0;i=i/10)     {         c=i%10;         s=s*10+c;     }     //printf("%d",s);     d=s*s;     for(j=d;j>0;j=j/10)     {         x=j%10;         y=y*10+x;     }     //printf("%d",y);     if(y==b)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Adam_number.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,i,s=0,d,j,x,y=0;
+    scanf("%d",&a);
+    b=a*a;
+    for(i=a;i>0;i=i/10)
+    {
+        c=i%10;
+        s=s*10+c;
+    }
+    //printf("%d",s);
+    d=s*s;
+    for(j=d;j>0;j=j/10)
+    {
+        x=j%10;
+        y=y*10+x;
+    }
+    //printf("%d",y);
+    if(y==b)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
