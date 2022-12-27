f3f62c97fe1df643a0df14b9fc74cf29559efe18
DivyaTejaP <119919278+DivyaTejaP@users.noreply.github.com>
2022-12-27 10:42:21 +0530

    #include<stdio.h> int main() {     int a[100],b,i,n,avg,s=0,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);         s=s+a[i];         avg=s/n;     }     for(i=0;i<n;i++)     {         if(avg<=a[i])         {             c++;         }     }     printf("%d",c); }

---
+++ elements_greater_than_average.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],b,i,n,avg,s=0,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        s=s+a[i];
+        avg=s/n;
+    }
+    for(i=0;i<n;i++)
+    {
+        if(avg<=a[i])
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
