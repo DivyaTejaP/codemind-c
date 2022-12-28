d05b9fab950ed44d79c8d80e920a0877f5e00dad
DivyaTejaP <119919278+DivyaTejaP@users.noreply.github.com>
2022-12-28 10:38:39 +0530

    #include<stdio.h> int main() {     int a,b,i,n,c,max=0;     scanf("%d",&n);     int arr[100];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d%d",&a,&b);     max=arr[a];      for(i=0;i<n;i++)     {         if(arr[i]<a || arr[i]>b)         {            if(max<arr[i])            {                c++;                max=arr[i];            }         }     }     if(c==0)     {         printf("-1");     }     else     {         printf("%d",max);     } }

---
+++ maximum_of_numbers_not_between_A_and_B.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,n,c,max=0;
+    scanf("%d",&n);
+    int arr[100];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d%d",&a,&b);
+    max=arr[a];
+     for(i=0;i<n;i++)
+    {
+        if(arr[i]<a || arr[i]>b)
+        {
+           if(max<arr[i])
+           {
+               c++;
+               max=arr[i];
+           }
+        }
+    }
+    if(c==0)
+    {
+        printf("-1");
+    }
+    else
+    {
+        printf("%d",max);
+    }
+}
