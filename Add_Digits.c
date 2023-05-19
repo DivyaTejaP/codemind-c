7cc683d32c19f0f5936e8693010c65bc3f4d1bd3
DivyaTejaP <119919278+DivyaTejaP@users.noreply.github.com>
2023-05-19 15:28:53 +0530

    #include<stdio.h> int ma(int a) {     int i,c,s=0;     for(i=a;i>0;i=i/10)     {         c=i%10;s=s+c;
        }
        if(a<10)
        {
            return s;
            
        }
        else
        {
            ma(s);
            
        }
        
    }
    int main()
    {
        int a;
        scanf("%d",&a);
        int h=ma(a);
        printf("%d",h);
        
    }

--- Add_Digits.c
+++ Add_Digits.c
@@ -1,23 +1,29 @@
 #include<stdio.h>
-int add(int a)
+int ma(int a)
 {
-    int i,s=0,b;
+    int i,c,s=0;
     for(i=a;i>0;i=i/10)
     {
-        b=i%10;
-    s=s+b;
+        c=i%10;s=s+c;
+        
     }
-    if(s<10)
-    return s;
+    if(a<10)
+    {
+        return s;
+        
+    }
     else
     {
-        add(s);
+        ma(s);
+        
     }
+    
 }
 int main()
 {
     int a;
     scanf("%d",&a);
-    int h=add(a);
+    int h=ma(a);
     printf("%d",h);
+    
 }
